5abb4318b07717c0bbed25b50a2777c07e768aa3
DevTotal <gonzalo.fernandezc@sansano.usm.cl>
2019-12-30 01:06:55 -0300

    Tarea Terminada :'D

--- RabbitMQ/cliente.py
+++ RabbitMQ/cliente.py
@@ -5,47 +5,47 @@ import threading
 import sys
 import pika
 import uuid 
+import json
 
 from datetime import datetime # interesante
 
 class Cliente:
 
 	def __init__(self, host, puerto):
+		self.host = host
+		self.puerto = puerto
 		self.nombre = ""
-		self.conn = pika.BlockingConnection(pika.ConnectionParameters(host, puerto))
-		self.channel = self.conn.channel()
+		self.conn = pika.BlockingConnection(pika.ConnectionParameters(host=host, port=puerto, heartbeat=0))
+		self.channelLogin = self.conn.channel()
+		self.channelServices = self.conn.channel()
+		self.channelChat = self.conn.channel()
+		self.uid = str(uuid.uuid4())
+		self.id = 0
+		self.key = ""
+		self.channelLogin.exchange_declare(exchange='login', exchange_type='direct')
+		self.channelServices.exchange_declare(exchange='services', exchange_type='direct')
+		self.channelChat.exchange_declare(exchange='chat', exchange_type='direct')
 
-		self.channel.exchange_declare(exchange='login', exchange_type='direct')
-		self.channel.exchange_declare(exchange='chat', exchange_type='direct')
-		
-		#threading.Thread(target=self.recibirMensajes, daemon=True).start()
 		print("[Info] Conexión establecida con el servidor:", host + ':' + str(puerto))
-		print("Mensaje enviado")
 
 		self.Login()
-		#self.connection.close()
+		print("[Info] Sesión iniciada.")
+		
+		threading.Thread(target=self.recibirMensajes, daemon=True).start()
+		self.enviarMensajes()
 
-		#self.cliente = chat.Cliente()
-		#threading.Thread(target=self.recibirMensajes, daemon=True).start()
-		#print("[Info] Sesión iniciada.")
-	
-		#self.enviarMensajes()
+	def mensajesCallBack(self, ch, method, properties, body):
+		data = json.loads(body)
+		print("[" + data["timestamp"] + "]", data["nombre"] + ":", data["valor"])
 
-	def callback(self, ch, method, properties, body):
-		print(" [x] %r:%r" % (method.routing_key, body))
-
-
-#	def recibirMensajes(self):
-#		result = self.channel.queue_declare(queue='', exclusive=True)
-#		queue_name = result.method.queue
-#		self.channel.queue_bind(exchange='chat', queue=queue_name, routing_key='login2')
-#
-#		print('[*] Waiting for logs. To exit press CTRL+C')
-#
-#		self.channel.start_consuming()
-#
-#		for mensaje in self.conn.Chat(self.cliente):
-#			print("[" + mensaje.timestamp + "]", mensaje.emisor.nombre + ":", mensaje.valor)
+	def recibirMensajes(self):
+		conn = pika.BlockingConnection(pika.ConnectionParameters(self.host, self.puerto))
+		channel = conn.channel()
+		colaChats = self.CrearCola(channel)
+		channel.exchange_declare(exchange='chat', exchange_type='direct')
+		channel.queue_bind(exchange='chat', queue=colaChats, routing_key=self.key)
+		channel.basic_consume(queue=colaChats, on_message_callback=self.mensajesCallBack, auto_ack=True)
+		channel.start_consuming()
 
 	def Ayuda(self):
 		print("[Info] El servidor de chat dispone de los siguientes 3 comandos:")
@@ -53,42 +53,35 @@ class Cliente:
 		print("\thistorial - Retorna la lista de mensajes que ha enviado")
 		print("\tmsg [idCliente] [mensaje] - Envía un mensaje al cliente con la id ingresada (sin corchetes)")
 
+	def CrearCola(self, channel):
+		return channel.queue_declare(queue='', exclusive=True).method.queue
+
 	def Login(self):
+		colaLogin = self.CrearCola(self.channelLogin)
 		error = 1
-		msg = "1,"
-		uid = str(uuid.uuid4())
-
-		result = self.channel.queue_declare(queue='', exclusive=True)
-		queue_name = result.method.queue
-
-		while self.nombre == "" and error:
+		data = {"uid": self.uid, "action": "login"}
+		while self.nombre == "" or error:
 			self.nombre = input("[Info] Ingrese su nombre para acceder al chat: ")
-			msg += uid + ","
-			msg += self.nombre	
-			key = uid+self.nombre
-			key = str(key)
-			#t1 = threading.Thread(target=self.resLogin, daemon=True)
-			#t1.start()
-			#t1.join()
+			data["key"] = self.uid + self.nombre[:15]
+			data["nombre"] = self.nombre
 
-			self.channel.queue_bind(exchange='chat', queue=queue_name, routing_key=key)
-			self.channel.basic_publish(exchange='login', routing_key='login', body=msg)
-			
+			self.channelLogin.queue_bind(exchange='login', queue=colaLogin, routing_key=data["key"])
+			self.channelLogin.basic_publish(exchange='login', routing_key='login', body=json.dumps(data))
 		
-			for _, _, res in self.channel.consume(queue_name):
-				print(res)
-				self.channel.cancel()
-				break
+			_, _, res =  next(self.channelLogin.consume(colaLogin))
+			res = json.loads(res)
 
-			print(res)
-			#res = #self.conn.Login(msg)
-			#error = res.error
-			#if error:
-			#	print(res.errorMensaje)
-		
-		#self.cliente.nombre = res.cliente.nombre
-		#self.cliente.id = res.cliente.id
-	"""
+			if "error" in res:
+				print(res["error"])
+			else:
+				error = 0
+
+		self.channelLogin.queue_unbind(exchange='login', queue=colaLogin, routing_key=data["key"])
+		self.key = data["key"]
+		self.id = res["id"]
+		self.colaServices = self.CrearCola(self.channelServices)
+		self.channelServices.queue_bind(exchange='services', queue=self.colaServices, routing_key=data["key"])
+	
 	def MensajeEnviado(self, r):
 		if r.result().error == 1:
 			print(r.result().errorMensaje)
@@ -99,29 +92,39 @@ class Cliente:
 
 		while texto != "salir":
 			data = texto.split()
+			enviar = {"key": self.key}
 			#try:
 			if data[0] == "msg":
-				mensaje = chat.Mensaje()
-				mensaje.receptor.id = int(data[1])
-				mensaje.emisor.id = self.cliente.id
-				mensaje.emisor.nombre = self.cliente.nombre
-				mensaje.valor = data[2]
-				mensaje.timestamp = datetime.now().strftime("%d-%b-%Y %H:%M:%S")
-				#res = self.conn.EnviarMensaje(mensaje)
-				self.conn.EnviarMensaje.future(mensaje).add_done_callback(self.MensajeEnviado)
-
+				enviar["id"] = self.id
+				enviar["receptor"] = int(data[1])
+				enviar["nombre"] = self.nombre
+				enviar["valor"] = data[2]
+				enviar["timestamp"] = datetime.now().strftime("%d-%b-%Y %H:%M:%S")
+				self.channelChat.basic_publish(exchange='chat', routing_key="chat", body=json.dumps(enviar))
 			elif data[0] == "clientes":
 				# No logré hacer asíncrona esta parte :c
-				print("[Server] Lista de clientes conectados al servidor de chat")
-				for i in self.conn.ListaClientes(chat.MensajeVacio()):
-					print(i.id, i.nombre) 
+				enviar["accion"] = "1"
+				self.channelServices.basic_publish(exchange='services', routing_key="services", body=json.dumps(enviar))
+				
+				_, _, res = next(self.channelServices.consume(self.colaServices))
+
+				res = json.loads(json.loads(res))
+
+				for i in res:
+					print(i[1], i[0])
+
 			elif data[0] == "historial":
 				print("[Server] Lista de mensajes enviados al servidor de chat")
-				mensaje = chat.Cliente()
-				mensaje.id = self.cliente.id
+				
+				enviar["accion"] = "2"
+				enviar["id"] = self.id
+				self.channelServices.basic_publish(exchange='services', routing_key="services", body=json.dumps(enviar))
+				
+				_, _, res =  next(self.channelServices.consume(self.colaServices))
+				res = json.loads(str(res.decode('UTF-8')))
 
-				for i in self.conn.ObtenerMensajes(mensaje):
-					print("[" + i.timestamp + "] (" + str(i.id) + ") -", i.receptor.nombre + ":", i.valor) 
+				for i in res:
+					print("[" + i[3] + "] (" + str(i[0]) + ") -", i[2] + ":", i[4]) 
 			else: 
 				self.Ayuda()
 			#except:
@@ -131,8 +134,9 @@ class Cliente:
 			texto = input()
 		else: # Sí, esto es tan legal como el yield
 			print("[Info] Hasta pronto!")
+			self.conn.close()
 			exit(0)
-	"""
+	
 
 if __name__ == '__main__':
 	if len(sys.argv) < 3:
--- RabbitMQ/server.py
+++ RabbitMQ/server.py
@@ -4,60 +4,125 @@
 import sys
 import pika
 import time
-
-from concurrent import futures
-
-def find_nth(haystack, needle, n):
-	start = haystack.find(needle)
-	while start >= 0 and n > 1:
-		start = haystack.find(needle, start+len(needle))
-		n -= 1
-	return start
+import json
+import threading
 
 class ChatServer():
 
 	def __init__(self, host, puerto):
-		self.conn = pika.BlockingConnection(pika.ConnectionParameters(host, puerto))
-		print("[Info] Iniciando servidor en ", host + ':' + str(puerto))
+		print("[Info] Conectando servidor a ", host + ':' + str(puerto))
+		self.host = host
+		self.puerto = puerto
 
-		self.channel = self.conn.channel()
-		self.channel.exchange_declare(exchange='login', exchange_type='direct')
-		self.channel.exchange_declare(exchange='chat', exchange_type='direct')
+		self.ultimoMensaje = 0 			  	 # id mensajes
+		self.chats = [ ] 				 	 # Chats
+		self.AgregarCliente("Broadcast", "") # Mensaje a todos
 
-		result = self.channel.queue_declare(queue='', exclusive=True)
-		queue_name = result.method.queue
-		self.channel.queue_bind(exchange='login', queue=queue_name, routing_key='login')
+		t1 = threading.Thread(target=self.handleServices, daemon=True)
+		t2 = threading.Thread(target=self.handleChats, daemon=True)
+		t3 = threading.Thread(target=self.handleLogin, daemon=True)
+		t1.start()
+		t2.start()
+		t3.start()
+		
+		while True:	# Perdón, no se me ocurrió una mejor forma de mantener abierto el servidor :c
+			continue
 
-		self.channel.basic_consume(queue=queue_name, on_message_callback=self.callback, auto_ack=True)
-		self.channel.start_consuming()
+	def handleServices(self):
+		self.connServices = pika.BlockingConnection(pika.ConnectionParameters(host=self.host, port=self.puerto, heartbeat=0))
+		self.channelServices = self.connServices.channel()
+		self.colaServices = self.channelServices.queue_declare(queue='', exclusive=True).method.queue
+		self.channelServices.exchange_declare(exchange='services', exchange_type='direct')
+		self.channelServices.queue_bind(exchange='services', queue=self.colaServices, routing_key='services')
 
-		#self.channel.exchange_declare(exchange='broadcasting', exchange_type='fanout')
+		self.channelServices.basic_consume(queue=self.colaServices, on_message_callback=self.servicesCallback, auto_ack=True)
+		self.channelServices.start_consuming()
 
-		#self.ultimoMensaje = 0 # id mensajes
-		#self.chats = [ ] # Chats
-		#self.AgregarCliente("Broadcast") # Mensaje a todos
+	def handleChats(self):
+		self.connChat = pika.BlockingConnection(pika.ConnectionParameters(self.host, self.puerto))
+		self.channelChat = self.connChat.channel()
+		self.colaChat = self.channelChat.queue_declare(queue='', exclusive=True).method.queue
+		self.channelChat.exchange_declare(exchange='chat', exchange_type='direct')
+		self.channelChat.queue_bind(exchange='chat', queue=self.colaChat, routing_key='chat')
+
+		self.channelChat.basic_consume(queue=self.colaChat, on_message_callback=self.chatsCallback, auto_ack=True)
+		self.channelChat.start_consuming()
+
+	def handleLogin(self):
+		self.connLogin = pika.BlockingConnection(pika.ConnectionParameters(self.host, self.puerto))
+		self.channelLogin = self.connLogin.channel()
+		self.colaLogin = self.channelLogin.queue_declare(queue='', exclusive=True).method.queue
+		self.channelLogin.exchange_declare(exchange='login', exchange_type='direct')
+		self.channelLogin.queue_bind(exchange='login', queue=self.colaLogin, routing_key='login')
+
+		self.channelLogin.basic_consume(queue=self.colaLogin, on_message_callback=self.loginCallback, auto_ack=True)
+		self.channelLogin.start_consuming()
+
+	def loginCallback(self, ch, method, properties, body):
+		body = json.loads(str(body.decode('UTF-8')))
+		print(body)
+		
+		res = {}
+
+		if self.ClienteRegistrado(body["nombre"]):
+			res["error"] = "[Error] Este nombre ya esta en uso"
+			print("[Error] Intento de acceso con un nombre en uso")
+		else:
+			res["id"] = self.AgregarCliente(body["nombre"], body["key"])
+		
+		self.channelLogin.basic_publish(exchange='login', routing_key=body["key"], body=json.dumps(res))
+	
+	def servicesCallback(self, ch, method, properties, body):
+		body = json.loads(str(body.decode('UTF-8')))
+		print(body)
+
+		if body["accion"] == "1":
+			print("[Info] Se solicitó la Lista de Clientes.")
+			res = json.dumps([[c[0], c[3]] for c in self.chats])
+			print(res)
+			self.channelServices.basic_publish(exchange='services', routing_key=body["key"], body=json.dumps(res))
+		elif body["accion"] == "2":
+			print("[Info] El cliente [" + self.chats[body["id"]][0] + "] solicitó su lista de mensajes.")
+			self.channelServices.basic_publish(exchange='services', routing_key=body["key"], body=json.dumps(self.chats[body["id"]][1][0]))
+
+	def chatsCallback(self, ch, method, properties, body):
+		body = json.loads(str(body.decode('UTF-8')))
+		print(body)
+		
+		res = {}
+		if body["receptor"] > len(self.chats):
+			res["error"] = "[Error] Receptor no valido"
+		else:
+			mensaje = [self.ultimoMensaje, self.chats[body["id"]][0], self.chats[body["receptor"]][0], body["timestamp"], body["valor"]]
+
+			self.NuevoMensaje(0, body["id"], mensaje)
+			self.NuevoMensaje(1, body["receptor"], mensaje)
+
+			self.channelChat.basic_publish(exchange='chat', routing_key=self.chats[body["receptor"]][2], body=json.dumps(body))
+
+			# "Broadcasting" 
+			if self.chats[body["receptor"]][3] == 0:
+				for cliente, _, key, cId in self.chats:
+					self.NuevoMensaje(1, cId, mensaje)
+					self.channelChat.basic_publish(exchange='chat', routing_key=key, body=json.dumps(body))
+
+			self.LogChat(mensaje[3], mensaje[0], mensaje[1], mensaje[2], mensaje[4])
+
+		return res
 
 
-	def callback(self, ch, method, properties, body):
-		body = str(body.decode('UTF-8'))
-		print(" [x] %r:%r" % (method.routing_key, body))
-		key = body[find_nth(body, ",", 1) + 1::].replace(",", "", 1)
-		print(key)
-		time.sleep(5)
-		self.channel.basic_publish(exchange='chat', routing_key=str(key), body='logeXXXXando')
-		print("Mensaje enviado")
+	def AgregarCliente(self, nombre, key):
+		# userId : [nombre, [msgEnviado, msgRecibido], key]
+		print("[Info] Cliente registrado - id:", str(len(self.chats)) + ", nombre:", nombre)
+		userId = len(self.chats)
+		self.chats.append( [nombre, [[], []], key, userId] )
+		return userId
 
-	"""
-	def AgregarCliente(self, nombre):
-		cliente = chat.Cliente()
-		cliente.nombre = nombre
-		cliente.id = len(self.chats)
-
-		# userId : [Cliente, [msgEnviado, msgRecibido]]
-		self.chats.append( [cliente, [[], []]] )
-
-		print("[Info] Cliente registrado - id:", str(cliente.id) +", nombre:", cliente.nombre)
-		return cliente
+	def ClienteRegistrado(self, nombre):
+		for nombreCliente, _, _, _ in self.chats:
+			if nombreCliente == nombre:
+				return True
+		return False
 
 	def LogChat(self, timestamp, id, emisor, receptor, mensaje):
 		logs = open("logs.txt", "a+")
@@ -68,70 +133,6 @@ class ChatServer():
 		logs.write(data)
 		logs.close()
 
-	def ClienteRegistrado(self, nombre):
-		for cliente, _ in self.chats:
-			if cliente.nombre == nombre:
-				registrado = 1
-				return True
-		return False
-
-	def Login(self, request: chat.Registro, context):
-		res = chat.RegistroRespuesta()
-		res.error = 0
-
-		if self.ClienteRegistrado(request.nombre):
-			res.error = 1
-			res.errorMensaje = "[Error] Este nombre ya esta en uso"
-			print("[Error] Intento de acceso con un nombre en uso")
-		else:
-			cliente = self.AgregarCliente(request.nombre)
-			res.cliente.nombre = cliente.nombre
-			res.cliente.id = cliente.id
-
-		return res
-
-	def ObtenerMensajes(self, request: chat.Cliente, context):
-		print("[Info] El cliente [" + self.chats[request.id][0].nombre + "] solicitó su lista de mensajes.")
-		for mensaje in self.chats[request.id][1][0]:
-			yield mensaje
-
-	def ListaClientes(self, request:chat.MensajeVacio, context):
-		print("[Info] Se solicitó la Lista de Clientes.")
-		for cliente, _ in self.chats:
-			yield cliente
-
-	def Chat(self, request: chat.Cliente, context):
-		ultimoLeido = 0
-		while True: # Querido CPU, lo lamento mucho, pero no se me ocurrió algo mejor :c
-			while ultimoLeido < len(self.chats[request.id][1][1]):
-				mensaje = self.chats[request.id][1][1][ultimoLeido]
-				ultimoLeido += 1
-				yield mensaje
-
-	def EnviarMensaje(self, r: chat.Mensaje, context):
-		res = chat.MensajeRecibido()
-		res.error = 0
-
-		if r.receptor.id > len(self.chats):
-			res.error = 1
-			res.errorMensaje = "[Error] Receptor no valido"
-		else:
-			r.id = self.ultimoMensaje
-			r.emisor.nombre = self.chats[r.emisor.id][0].nombre
-			r.receptor.nombre = self.chats[r.receptor.id][0].nombre
-
-			self.NuevoMensaje(0, r.emisor.id, r)
-			self.NuevoMensaje(1, r.receptor.id, r)
-
-			# "Broadcasting" 
-			if r.receptor.id == 0:
-				for cliente, _ in self.chats:
-					self.NuevoMensaje(1, cliente.id, r)
-
-			self.LogChat(r.timestamp, r.id, self.chats[r.emisor.id][0].nombre, self.chats[r.receptor.id][0].nombre, r.valor)
-
-		return res
-
 	def NuevoMensaje(self, tipo, cliente, mensaje):
 		# tipo = 0 -> Cliente envió 
 		# tipo = 1 -> Cliente recibió
@@ -139,8 +140,7 @@ class ChatServer():
 		if tipo == 0:
 			self.ultimoMensaje += 1
 
-		self.chats[cliente][1][tipo].append(mensaje)
-	"""
+		self.chats[int(cliente)][1][tipo].append(mensaje)
 
 if __name__ == '__main__':
 	if len(sys.argv) < 3:
